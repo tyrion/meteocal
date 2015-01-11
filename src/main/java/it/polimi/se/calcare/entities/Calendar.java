f941dca618ebbf356687c6d0a599e14fd0babf1d
Germano Gabbianelli <tyrion.mx@gmail.com>
2015-01-11 20:15:29 +0100

    Fixed the FU****G 500-NOLOGS error.
    Was caused by deserialization infinite loop.

--- src/main/java/it/polimi/se/calcare/entities/Calendar.java
+++ src/main/java/it/polimi/se/calcare/entities/Calendar.java
@@ -22,6 +22,7 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
+import org.eclipse.persistence.oxm.annotations.XmlInverseReference;
 
 /**
  *
@@ -47,6 +48,7 @@ public class Calendar implements Serializable {
 
     @JoinColumn(name = "owner", referencedColumnName = "id")
     @OneToOne(optional = false)
+    @XmlInverseReference(mappedBy="calendar")
     private User owner;
 
     
