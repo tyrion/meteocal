5627ba4625e89fb8251c0f5389f0fe68fe91fc5c
Germano Gabbianelli <tyrion.mx@gmail.com>
2015-01-16 00:31:16 +0100

    Enabled mapping of Exceptions to JSON.
    Needs improvement.

---
+++ src/main/java/it/polimi/se/calcare/JSONExceptionMapper.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2015 Germano Gabbianelli
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package it.polimi.se.calcare;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+/**
+ *
+ * @author Germano Gabbianelli
+ */
+@Provider
+public class JSONExceptionMapper implements ExceptionMapper<Exception> {
+
+    @Override
+    public Response toResponse(Exception exception) {
+        ResponseBuilder response;
+        if (exception instanceof WebApplicationException) {
+            WebApplicationException ex = (WebApplicationException) exception;
+            response = Response.status(ex.getResponse().getStatus());
+        } else {
+            response = Response.status(500);
+        }
+        // FIXME: this is not escaped.
+        return response.entity(
+                String.format("{\"error\": \"%s\"}",
+                        exception)).build();
+    }
+
+    private static String getStackTrace(final Throwable throwable) {
+        final StringWriter sw = new StringWriter();
+        final PrintWriter pw = new PrintWriter(sw, true);
+        throwable.printStackTrace(pw);
+        return sw.getBuffer().toString();
+    }
+}
