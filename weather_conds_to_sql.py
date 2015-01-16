f9a8398ce5e7c0452f7f683777d18fe7d9c2802a
Andrea Grazioso <grazioandre@gmail.com>
2015-01-16 12:01:39 +0100

    Trying to make things working with the sql file

--- weather_conds_to_sql.py
+++ weather_conds_to_sql.py
@@ -1,6 +1,6 @@
-def getQuery(id, name, icon):
-    return "INSERT INTO `meteocal`.`weather_conditions` (`id`, `name`, `description`, `icon`) VALUES ('%s', '%s', '', '%s');" % (id, name, icon)
+def getQuery(id, name, description, icon):
+    return "INSERT INTO `meteocal`.`weather_conditions` (`id`, `name`, `description`, `icon`) VALUES ('%s', '%s', '%s', '%s');" % (id, name, description, icon)
 
 with open("WeatherConditions.txt","r") as f, open("weatherConditionsInsert.sql", "w") as g:
     lines = [(i, x.strip('\n')) for i, x in enumerate(f.readlines())]
-    [g.write(getQuery(id, name, icon) + '\n') for (i, id), (j, name), (k, icon) in zip(*map(lambda y: filter(lambda x: x[0] % 3 == y, lines), [0, 1, 2]))]
+    [g.write(getQuery(id, name, description, icon) + '\n') for (i, id), (j, name), (k, description), (w, icon) in zip(*map(lambda y: filter(lambda x: x[0] % 4 == y, lines), [0, 1, 2, 3]))]
