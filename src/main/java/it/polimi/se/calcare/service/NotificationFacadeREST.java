b4f7b5cfc6adbc808217a3028140baedf986bb13
Germano Gabbianelli <tyrion.mx@gmail.com>
2015-01-24 15:28:22 +0100

    Fixed NotificationFacadeREST.findAll.

--- src/main/java/it/polimi/se/calcare/service/NotificationFacadeREST.java
+++ src/main/java/it/polimi/se/calcare/service/NotificationFacadeREST.java
@@ -6,18 +6,14 @@
 package it.polimi.se.calcare.service;
 
 import it.polimi.se.calcare.auth.AuthRequired;
-import it.polimi.se.calcare.entities.Event;
 import it.polimi.se.calcare.entities.Notification;
 import it.polimi.se.calcare.entities.User;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -32,23 +28,23 @@ import javax.ws.rs.core.SecurityContext;
 @Stateless
 @Path("notifications")
 public class NotificationFacadeREST extends AbstractFacade<Notification> {
+
     @PersistenceContext(unitName = "it.polimi.se_CalCARE_war_1.0-SNAPSHOTPU")
     private EntityManager em;
 
     public NotificationFacadeREST() {
         super(Notification.class);
     }
-    
+
     @GET
     @AuthRequired
     @Produces({"application/json"})
     public List<Notification> findAll(@Context SecurityContext sc) {
         User user = (User) sc.getUserPrincipal();
-        return em.createNamedQuery("Notification.findUserNotifs", Notification.class)
-                .setParameter("currentUser", user.getId())
-                .getResultList();
+        return em.createNamedQuery("Notification.findByUser", Notification.class)
+                .setParameter("user", user).getResultList();
     }
-    
+
     @AuthRequired
     @DELETE
     @Path("{id}")
@@ -64,5 +60,5 @@ public class NotificationFacadeREST extends AbstractFacade<Notification> {
     protected EntityManager getEntityManager() {
         return em;
     }
-    
+
 }
