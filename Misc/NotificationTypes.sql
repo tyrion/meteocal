ef9aaf13275402731a4d341ef02f452d2b998d1b
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2015-01-25 12:22:12 +0100

    Add STABLE sql for the system installation

---
+++ Misc/NotificationTypes.sql
@@ -0,0 +1,9 @@
+INSERT INTO meteocal.notifications_types (`name`, description) 
+	VALUES ('Event Creation', 'You''ve been invited to the event %s by %s. Link to the event: ');
+INSERT INTO meteocal.notifications_types (`name`, description) 
+	VALUES ('Bad Weather - 3 Days', 'For your event %s the forecast reports bad weather. We found the first available sunny day to be on %s. Edit your event at: ');
+INSERT INTO meteocal.notifications_types (`name`, description) 
+	VALUES ('Bad Weather - 3 Days - No sunny days', 'For your event %s the forecast reports bad weather. Unfortunately there isn''t any sunny day for the next days.');
+INSERT INTO meteocal.notifications_types (`name`, description) 
+	VALUES ('Bad Weather - Tomorrow!', 'There''s bad weather for your tomorrow event %s! Move it please: ');
+
